2f1290868fabf706c4fddd87aeedf676917e43c2
kinkar1990 <kinkarpaul347@gmail.com>
2021-02-27 17:15:25 +0530

    committed files

---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'bid-my-craft';
+  slides = [
+    {img: "../assets/images/banner-1.jpg"},
+    {img: "../assets/images/banner-1.jpg"},
+    {img: "../assets/images/banner-1.jpg"},
+    {img: "../assets/images/banner-1.jpg"}
+  ];
+  slideConfig = {
+    "slidesToShow": 4,
+    "slidesToScroll": 1,
+    "nextArrow": "<div class='nav-btn next-slide'></div>",
+    "prevArrow": "<div class='nav-btn prev-slide'></div>",
+    "dots": true,
+    "infinite": false
+  };
+  
+  addSlide() {
+    this.slides.push({img: "http://placehold.it/350x150/777777"})
+  }
+  
+  removeSlide() {
+    this.slides.length = this.slides.length - 1;
+  }
+  
+  slickInit(e) {
+    console.log('slick initialized');
+  }
+  
+  breakpoint(e) {
+    console.log('breakpoint');
+  }
+  
+  afterChange(e) {
+    console.log('afterChange');
+  }
+  
+  beforeChange(e) {
+    console.log('beforeChange');
+  }
+}
